b7b107d4c9f0cd978114b89aac5496665d5348d4
TheKrafter <75903664+TheKrafter@users.noreply.github.com>
2022-03-17 20:32:15 -0500

    script installs my programs, is not distro reliant

--- files/flatpak-cust.sh
+++ files/flatpak-cust.sh
@@ -1,2 +1,67 @@
 #!/bin/bash
-# This script will install all of the flatpak apps krafter uses
+# Customization Script for Linux Systems with Flatpak, by Krafter, and written in Bash.
+##################################################
+# (c) 2021 'Krafter' [https://www.github.com/TheKrafter]
+# Published Under the MIT License.
+##################################################
+# Version alpha-1.2
+# This, once complete should be able to install all the flatpak programs I need.
+WILL_INSTALL_FLATPAK='Inkscape, VSCodium, Nextcloud Sync, GitHub Desktop, OBS Studio, Kdenlive, Pithos, Discord, Fractal, Tor Browser, Minecraft.'
+# It will also put the mcpelauncher appimage in ~/Applications/mcpelauncher/ (as the flatpak no longer exists)
+MCPELAUNCHER_DL='https://github.com/ChristopherHX/linux-packaging-scripts/releases/download/v0.3.3-683/Minecraft_Bedrock_Launcher-x86_64-v0.3.3.683.AppImage'
+MCPELAUNCHER='Minecraft_Bedrock_Launcher-x86_64-v0.3.3.683.AppImage'
+echo 'This bash script by Krafter (thekrafter.github.io) will install some Flatpak programs for a freshly installed Linux system. (and 1 AppImage)'
+echo 'This script assumes you are on an x86_64 (or amd64) machine with flatpaks and flathub enabled.'
+echo 'Flatpak Apps to Install: $WILL_INSTALL_FLATPAK'
+echo
+read -p "Do you agree that Flatpak is the superior ? [Y/n]" -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo 'Installing org.inkscape.Inkscape'
+    flatpak install flathub org.inkscape.Inkscape
+    echo 'Installed.'
+    echo 'Installing com.vscodium.codium'
+    flatpak install flathub com.vscodium.codium
+    echo 'Installed.'
+    echo 'Installing com.nextcloud'
+    flatpak install flathub com.nextcloud
+    echo 'Installed.'
+    echo 'Installing io.github.shiftkey.Desktop'
+    flatpak install flathub io.shiftkey.Deskop
+    echo 'Installed.'
+    echo 'Installing com.obsproject.Studio'
+    flatpak install flathub com.obsproject.Studio
+    echo 'Installed.'
+    echo 'Installing org.kde.kdenlive'
+    flatpak install flathub org.kde.kdenlive
+    echo 'Installed.'
+    echo 'Installing io.github.Pithos'
+    flatpak install flathub io.github.Pithos
+    echo 'Installed.'
+    echo 'Installing com.discordapp.Discord'
+    flatpak install flathub com.discordapp.Discord
+    echo 'Installed.'
+    echo 'Installing org.gnome.Fractal'
+    flatpak install flathub org.gnome.Fractal
+    echo 'Installed.'
+    echo 'Installing com.github.micahflee.torbrowser-launcher'
+    flatpak install flathub com.github.micahflee.torbrowser-launcher
+    echo 'Installed.'
+    echo 'Installing com.mojang.Minecraft'
+    flatpak install flathub com.mojang.Minecraft
+    echo 'Installed.'
+    echo 'Installing MCPE Launcher.'
+    mkdir $HOME/Applications/mcpelauncher/
+    cd $HOME/Applications/mcpelauncher/
+    wget $MCPELAUNCHER_DL
+    chmod +x $MCPELAUNCHER
+    echo 'alias mcpe="$HOME/Applications/mcpelauncher/$MCPELAUNCHER' >> $HOME/.bashrc
+    echo 'mcpe is now aliased to run MCPE Launcher in your bashrc'
+    echo 'Installed.'
+    echo 'Installed '$WILL_INSTALL_FLATPAK
+else
+    echo "Well then, u dont like flatpaks? Who are you? A Snap user? Just No. Watch The Linux Experiment's video."
+    echo "Krafter Dissaproves."
+fi
+echo "Complete."
